f709271b100c2c0f1eae83b41952136c944e8b16
Atla004 <andrese.g.v.13579@gmail.com>
2025-02-13 12:09:02 -0400

    feat: initialize Vite project with configuration and .gitignore

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './', // o la carpeta donde estén tus archivos de entrada (index.html)
+  server: {
+    open: true
+  }
+});
